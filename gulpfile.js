84c948c49b354a3a17960e6ef0dd4cc9dbe71120
bjorne84 <edinbjorn@gmail.com>
2020-09-11 12:13:34 +0200

    nu verkar allt fungera som tänkt, css, bilder, html, js allt perfekt

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,9 @@ const uglify = require("gulp-uglify-es").default;
 const autoprefixer = require('gulp-autoprefixer');
 const sourcemaps = require('gulp-sourcemaps');
 const cleanCSS = require('gulp-clean-css');
+const browserSync = require('browser-sync').create();
+const imagemin = require('gulp-imagemin');
+const htmlmin = require('gulp-htmlmin');
 
 
 /* ---- Sökvägar ----
@@ -22,33 +25,43 @@ src = src katalogen
 const files = {
     htmlPath: "src/**/*.html",
     cssPath: "src/**/*.css",
-    jsPath: "src/**/*.js"
+    jsPath: "src/**/*.js",
+    imgPath: "src/images/*"
 }
 
-/* kopiera HTML-filer
+/* kopiera HTML-filer och plockar bort kommentarer
 för att indikera att att funktionen är klar läggs return till,
 den skall skickas vidare i flödet med pipe, skall flyttas från src-katalogen 
 till pup-katalogen*/
-function copyHTML() {
+function htmlTask() {
     return src(files.htmlPath)
+    .pipe(htmlmin({ removeComments: true }))
     .pipe(dest('pup')
     );
 }
 
 /* Sammanslå och minifiera JS-filer, i concat-stadiet så ligger den bara
-i minnet, den skrivs in dest-stadiet, alltså main.js i pup skrivs först då*/
+i minnet, den skrivs in dest-stadiet, alltså main.js i pup skrivs först då, sourcemap körs för att man 
+skall kunna se från vilken fil koden kommer*/
 function jsTask() {
     return src(files.jsPath)
+    .pipe(sourcemaps.init())
     .pipe(concat('main.js'))
     .pipe(uglify())
+    .pipe(sourcemaps.write('/.'))
     .pipe(dest('pup/js')
     );
 }
 
 
-/* Task/function för att hantera css-filer
-1. Slår samman css-filerna från src till en css-fil - .pipe(concat())
-2. */
+/* ------Task/funktion för att hantera css-filer -----
+1. Hämtar alla css filer med return src
+2. Startar igång sourcemap (som gör att man ser från vilken fil koden kommer i den sammanslagna)
+3. Slår samman css-filerna från src till en css-fil - .pipe(concat())
+4. Kör autoprefixer för automatisera inställningar för enskilda webbläsare
+5. Minifiera CSS och spotta ut console.log med info om besparingen av storlek
+6. avsluta sourcemaps
+7. skicka över filen till pup foldern*/
 function cssTask() {
     return src(files.cssPath)
     .pipe(sourcemaps.init())
@@ -64,11 +77,32 @@ function cssTask() {
 
 }
 
+/* Minifierar bildfiler genom att komprimera dem och sedan skicka till pup-folder*/
+function imgTask() {
+    return src(files.imgPath)
+    .pipe(imagemin())
+    .pipe(dest('pup/images')
+    );
+}
+
+
 
 /* kollar om den hittar ändringar och då sparkar den igång tasks*/
 function watchTask() {
-    watch([files.htmlPath, files.jsPath], 
-        parallel(copyHTML, jsTask));
+
+    /* Browser sync uppdaterar wepplatsen on the go*/
+    browserSync.init({
+        injectChanges: false,
+        server: {
+            baseDir: './pup'
+        }
+    });
+   
+    watch([files.htmlPath, files.jsPath, files.imgPath], 
+        parallel(htmlTask, jsTask, imgTask)).on('change', browserSync.reload);
+    watch(files.cssPath, cssTask).on('change', browserSync.reload);
+
+    
 }
 
 
@@ -77,7 +111,8 @@ eller kommandopromten*/
 
 //default task
 exports.default = series(
-    parallel(copyHTML, jsTask),
+    parallel(htmlTask, jsTask, cssTask, imgTask),
     watchTask
 );
+
 
