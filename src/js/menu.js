9c6ff4c1558ce61ac8881f987f0dc37113f90089
bjorne84 <edinbjorn@gmail.com>
2020-09-20 16:05:24 +0200

    allt klart

---
+++ src/js/menu.js
@@ -0,0 +1,18 @@
+(function() {
+
+    let hamburger = {
+      navToggle: document.querySelector('.nav-toggle'),
+      nav: document.querySelector('nav'),
+  
+      doToggle: function(e) {
+        e.preventDefault();
+        this.navToggle.classList.toggle('expanded');
+        this.nav.classList.toggle('expanded');
+      }
+    };
+  
+    hamburger.navToggle.addEventListener('click', function(e) { hamburger.doToggle(e); });
+    //tog bort nedanstående, då funkade ej länkarna
+    /*hamburger.nav.addEventListener('click', function(e) { hamburger.doToggle(e); });*/
+  
+  }());
